0304bf9f47b3b08ef6d2338adc4b9edb1449f741
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-08-04 12:32:00 +0800

    日志1

---
+++ ConsoleLogging/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleLogging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+ServiceCollection service=new ServiceCollection();
+//如何输出日志，由框架配置
+service.AddLogging(logBuilder =>
+{
+    logBuilder.AddConsole();
+    logBuilder.AddEventLog();
+    logBuilder.SetMinimumLevel(LogLevel.Debug);//最低输出那些级别的信息，这条可以通过文件配置
+});
+
+service.AddScoped<Test>();
+using var sp = service.BuildServiceProvider();
+var test=sp.GetRequiredService<Test>();
+test.TestDebug();
---
+++ ConsoleLogging/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleLogging
+{
+    public class Test
+    {
+        //使用当前类，在日志中提示记录日志的对象
+        private readonly ILogger<Test> _logger;
+        public Test(ILogger<Test> logger)
+        {
+            _logger = logger;
+        }
+
+        public void TestDebug()
+        {
+            //使用者无需关心日志输出的具体设置，只需要使用即可
+            _logger.LogDebug("Start");
+            _logger.LogDebug("Query");
+            _logger.LogWarning("Retry");
+            //模拟记录异常
+           try
+           {
+               File.ReadAllText("A:/1.txt");
+               _logger.LogDebug("Read file success");
+           }
+           catch (Exception e)
+           {
+               _logger.LogError(e, "Read file failed!");
+           }
+        }
+    }
+}
